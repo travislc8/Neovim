72442f4a44871c21e9884245722345e9bdc0781a
Travis Clark <157655849+travislc8@users.noreply.github.com>
2024-03-21 10:28:28 -0500

    Initial commit

---
+++ lua/tclark/init.lua
@@ -0,0 +1,5 @@
+require("tclark.remap")
+require("tclark.set")
+
+--Transparent background
+vim.g.moonflyTransparent = true
---
+++ lua/tclark/set.lua
@@ -0,0 +1,30 @@
+-- line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+--indent
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+--vim.opt.swapfile = false
+--vim.opt.backup = false
+--vim.opt.undofile = true
+--vim.opt.undodir = os.getenv("HOME") .. "/undodir"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+--keeps lines showing below cursor
+vim.opt.scrolloff = 8
+vim.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
